b6ffa5173e48bdfa0a39244d94cffde5a9660fb6
keplib <keplib@gmail.com>
2024-08-09 18:11:33 +0200

    get all entries from blog table

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Sequelize } = require('sequelize');
+const { Sequelize, QueryTypes } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.PROJECT_URL);
 
@@ -7,6 +7,8 @@ const main = async () => {
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
+    const notes = await sequelize.query('SELECT * FROM blogs', { type: QueryTypes.SELECT });
+    console.log(notes);
     sequelize.close();
   } catch (error) {
     console.error('Unable to connect to the database:', error);
