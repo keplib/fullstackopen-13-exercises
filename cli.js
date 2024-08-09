36ed75d1f596c01bc348fa89f9ff6fa6abc69c18
keplib <keplib@gmail.com>
2024-08-09 18:21:46 +0200

    13.3 solution

--- cli.js
+++ cli.js
@@ -8,7 +8,9 @@ const main = async () => {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
     const notes = await sequelize.query('SELECT * FROM blogs', { type: QueryTypes.SELECT });
-    console.log(notes);
+    notes.forEach((note) => {
+      console.log(`${note.author}: '${note.content}', ${note.likes} likes`);
+    });
     sequelize.close();
   } catch (error) {
     console.error('Unable to connect to the database:', error);
