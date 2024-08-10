f0a4ced86b6b776c68a1e017750195b5c0ef2e25
keplib <keplib@gmail.com>
2024-08-10 19:24:33 +0200

    13.4 change to Sequlize ORM commands instead of SQL queries

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 require('dotenv').config();
-const { Sequelize, QueryTypes } = require('sequelize');
+const { Sequelize, QueryTypes, Model, DataTypes } = require('sequelize');
 const express = require('express');
 const app = express();
 
 const sequelize = new Sequelize(process.env.PROJECT_URL);
 
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog',
+  }
+);
+
 app.get('/api/blogs', async (req, res) => {
-  const notes = await sequelize.query('SELECT * FROM blogs', { type: QueryTypes.SELECT });
-  res.json(notes);
+  //   const notes = await sequelize.query('SELECT * FROM blogs', { type: QueryTypes.SELECT });
+  //     res.json(notes);
+  const blogs = await Blog.findAll();
+  res.json(blogs);
 });
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
